4b7c5d5a911c5d440e5ebc8ea83e96f283f3ba27
Christian Egwim <emeksense@gmail.com>
2019-03-17 06:44:25 +0100

    Allowing the function to return availabilities on as many days as requested with default number 7 incase number of availability was not specified

--- src/getAvailabilities.js
+++ src/getAvailabilities.js
@@ -1,9 +1,9 @@
 import moment from "moment";
 import knex from "knexClient";
 
-export default async function getAvailabilities(date) {
+export default async function getAvailabilities(date, numberOfDays = 7) {
   const availabilities = new Map();
-  for (let i = 0; i < 7; ++i) {
+  for (let i = 0; i < numberOfDays; ++i) {
     const tmpDate = moment(date).add(i, "days");
     availabilities.set(tmpDate.format("d"), {
       date: tmpDate.toDate(),
@@ -14,15 +14,13 @@ export default async function getAvailabilities(date) {
   const events = await knex
     .select("kind", "starts_at", "ends_at", "weekly_recurring")
     .from("events")
-    .where(function() {
+    .where(function () {
       this.where("weekly_recurring", true).orWhere("ends_at", ">", +date);
     });
 
   for (const event of events) {
     for (
-      let date = moment(event.starts_at);
-      date.isBefore(event.ends_at);
-      date.add(30, "minutes")
+      let date = moment(event.starts_at); date.isBefore(event.ends_at); date.add(30, "minutes")
     ) {
       const day = availabilities.get(date.format("d"));
       if (event.kind === "opening") {
