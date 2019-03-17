3facd48fe28d88ba96f71fcdb196a5eb5cc27eab
Christian Egwim <emeksense@gmail.com>
2019-03-17 07:08:14 +0100

    Implemented post request for the creation of absence for persons, group and wholeteams

--- src/doctorlibAPI.js
+++ src/doctorlibAPI.js
@@ -41,6 +41,50 @@ app.get("/api/wholeTeams", (req, res) => {
     res.send(wholeTeams);
 });
 
+// Http post - For Creating absence
+app.post("/api/persons", (req, res) => {
+    const result = validatePersons(req.body);
+    if (result.error) {
+        return res.send(400, result.error.details[0].message);
+    }
+
+    const person = {
+        id: persons.length + 1,
+        reason: req.body.reason
+    };
+    books.push(person);
+    res.send(person);
+});
+
+app.post("/api/groups", (req, res) => {
+    const result = validateGroups(req.body);
+    if (result.error) {
+        return res.send(400, result.error.details[0].message);
+    }
+
+    const group = {
+        id: groups.length + 1,
+        reason: req.body.reason
+    };
+    books.push(group);
+    res.send(group);
+});
+
+app.post("/api/wholeTeams", (req, res) => {
+    const result = validateWholeTeams(req.body);
+    if (result.error) {
+        return res.send(400, result.error.details[0].message);
+    }
+
+    const wholeTeam = {
+        id: wholeTeams.length + 1,
+        reason: req.body.reason
+    };
+    books.push(wholeTeam);
+    res.send(wholeTeam);
+});
+
+
 // Validation using JOI
 function validatePersons(person) {
     const schema = {
